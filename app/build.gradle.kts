eb7e11cf88168a2635e20dbdb00e19239e3be6cd
WisdomAz <wisdom.azonwu@my.metrostate.edu>
2025-04-17 19:17:55 -0500

    Assignment 5: Add location‐based weather & persistent notification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("androidx.compose.material:material-icons-extended:1.5.1")
 
 
 
